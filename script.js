d6f26ae24e70f547e0def6d48f9d5284ad08e61b
Your Name <you@example.com>
2025-04-07 20:58:56 +0300

    Update

---
+++ script.js
@@ -0,0 +1,23 @@
+function showNotification({ top = 0, right = 0, html }) {
+  var notification = document.createElement("div");
+  notification.className = "notification";
+  document.body.append(notification);
+
+  if (top) {
+    notification.style.top = top + "px";
+  };
+
+  if (right) {
+    notification.style.right = right + "px";
+  };
+
+  if (html) {
+    notification.innerHTML = `<p>${html}</p>`;
+  };
+
+  setTimeout(() => {
+    notification.remove();
+  }, 1500);
+}
+
+showNotification({top: 10, right: 10, html: "Notification!",});
